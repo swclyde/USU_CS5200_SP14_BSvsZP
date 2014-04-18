5c5d849c0c608384de0ec8f9a285de6f46892a3d
Stephen W. Clyde <swc@mdsc.com>
2014-04-18 14:49:34 -0600

    Added a tester for FieldLayoutReply

---
+++ BSvsZP-Common/MessagesTester/FieldLayoutReplyTester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessagesTester
+{
+    [TestClass]
+    public class FieldLayoutReplyTester
+    {
+        [TestMethod]
+        public void FieldLayoutReply_TestEverything()
+        {
+
+        }
+    }
+}
